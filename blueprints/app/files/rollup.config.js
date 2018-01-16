b16e05c840c042e55c6427fbc6b924bba0ba8db5
Michael Nutt <michael@nuttnet.net>
2018-01-16 01:26:20 -0500

    upgrade rollup-split-index and rollup config

--- blueprints/app/files/rollup.config.js
+++ blueprints/app/files/rollup.config.js
@@ -3,30 +3,40 @@ const rollup = require("rollup");
 
 const resolve = require("rollup-plugin-node-resolve");
 const commonjs = require("rollup-plugin-commonjs");
+const serve = require("rollup-plugin-serve");
 
-const {
-  importExportToGlobal,
-  dependenciesOnly
-} = require("rollup-split-index");
+const { importExportToGlobal, dependenciesOnly } = require("rollup-split-index");
 
 const inputFile = config.main; // likely index.js
 
-module.exports = [
-  {
-    input: inputFile,
-    plugins: [resolve(), commonjs(), dependenciesOnly()],
-    output: {
-      name: importExportToGlobal.referenceName,
-      file: "dist/vendor.js",
-      format: "iife"
-    }
-  },
-  {
-    input: inputFile,
-    plugins: [resolve(), commonjs(), importExportToGlobal()],
-    output: {
-      file: "dist/index.js",
-      format: "es"
-    }
+const vendorTree = {
+  // only used to determine which files to include in vendor tree
+  input: inputFile,
+  plugins: [resolve(), commonjs(), dependenciesOnly()],
+  output: {
+    // Vendor dependencies will be stored in the global namespace under this variable name
+    name: "studioDependencies",
+    file: "dist/vendor.js",
+    format: "iife"
   }
-];
+};
+
+const indexTree = {
+  input: inputFile,
+  plugins: [
+    resolve(),
+    commonjs(),
+    importExportToGlobal({ importName: vendorTree.output.name })
+  ],
+  output: {
+    file: "dist/index.js",
+    // output format is es6, but with imports/exports rewritten to es5
+    format: "es"
+  }
+};
+
+if (process.env.SERVE) {
+  vendorTree.plugins.push(serve());
+}
+
+module.exports = [vendorTree, indexTree];
